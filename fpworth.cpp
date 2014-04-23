cd920454951e2afad446bff2d8aa1a1992c9b3b2
Michael <kmi9.other@gmail.com>
2014-04-23 11:00:38 +0400

    test2

--- fpworth.cpp
+++ fpworth.cpp
@@ -16,7 +16,6 @@
 // Apriori можно ускорить. Бизнес-аналитика От данных к знаниям 2013.djvu, стр. 290.
 // Вместо обращения к базе, просмотр наборов размера k-1.
 
-
 QString QString::number(double n, char f, int prec){
     return QLocale().toString(n, f, prec);
 }
